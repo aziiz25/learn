7ad621dbc8cd26c84a58b459e84a797baa0e45b9
be5her <besher.karam.1998@hotmail.com>
2021-06-27 17:32:00 +0300

    that is better

--- code.js
+++ code.js
@@ -1,14 +1,11 @@
 function lastFunction() {
     console.log("last");
 }
+
 function frstFunction() {
     console.log("vchjucvu")
 }
 
 function noIamFirstFunction() {
     console.log("hi i am First yeeeeeeeeey");
-}
-
-function noIamFirstFunction() {
-    console.log("hi i am bkjgbigFirst");
 }
